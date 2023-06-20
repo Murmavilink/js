ca000f8e7ec73b8a63d453f2a62797423735397a
Murad <murmavilink@gmail.com>
2023-06-20 22:51:43 +0600

    done lesson-1

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 'use strict';
 
 
+const num = 266219;
+// const num = 123;
+const numbers = Array.from(String(num));
+
+// console.log(String(num).split(''));
+// console.log(Array.from(String(num)));
+
+const sum = numbers.reduce((currentSum, currentNumber) => {
+    return currentSum * currentNumber;
+
+    // console.log(currentSum);
+    console.log(currentNumber);
+}, 1);
+
+// console.log(sum ** 3);
+const sumPow = sum ** 3;
+
+console.log(String(sumPow).substring(0, 2));
+
+// ////////////////////
+// let res = 0;
+
+// numbers.forEach(num => {
+//     res = res + +num;
+//     // res += +num;
+// });
+
+// console.log(res);
 
 
-let str = "Hello my Friends!";
+// ////////////////////
+let res = 1;
 
-console.log(str.substring(6));
-console.log(str.substring(6, 15));
-console.log(str.slice(-8));
+for(let i = 0; i < numbers.length; i++) {
+    res = res * numbers[i];
+}
+
+console.log(res);
